10db71b001c4ccdd10d36dd6a0175b9514ac8c42
yujin3150 <mizusaki0406@gmail.com>
2023-09-05 21:57:50 +0900

    Changes

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CalcTheme {
-        Greeting("Android")
+        Greeting("Androida")
     }
 }
